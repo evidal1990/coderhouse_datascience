15e47aa2bf8c20e2f53378a15479e071b0520c33
Edith Vidal <ecsr.vidal@gmail.com>
2025-02-24 21:52:22 -0300

    Removendo código não utilizado da main

--- src/main.py
+++ src/main.py
@@ -3,20 +3,7 @@ import sys
 import pandas as pandas
 
 from dotenv import load_dotenv
-from graph.graph_generator import GraphGenerator
 from menu.main_menu import MainMenu
-
-CURRENT_COLUMN = "against_dark"
-
-menu_options = {
-    "graph": {
-        "exit": 0,
-        "boxplot": 1,
-        "swarmplot": 2,
-        "violinplot": 3,
-        "back_to_main_menu": 9,
-    }
-}
 
 
 def main():
@@ -32,45 +19,6 @@ def main():
 
     except Exception as exception:
         print(exception)
-
-
-def init_graph_menu():
-    print("\nMENU GRÁFICOS")
-    print("0-Sair")
-    print("1-Boxplot")
-    print("2-Swarmplot")
-    print("3-Violinplot")
-    print("9-Voltar ao menu inicial")
-
-    return input("Escolha uma opção: ")
-
-
-def init_graph_generation(df):
-    show_menu = True
-    while (show_menu):
-        menu_option = init_graph_menu()
-        graph_generator = GraphGenerator()
-
-        if (menu_option == "2"):
-            column_name_y = input("Informe uma coluna: ")
-            graph_generator.draw_swarmplot(
-                dataframe=df,
-                column=column_name_y
-            )
-
-        if (menu_option == "3"):
-            column_name_y = input("Informe uma coluna: ")
-            graph_generator.draw_violinplot(
-                dataframe=df,
-                column=column_name_y
-            )
-
-        if (menu_option == "9"):
-            show_menu = False
-            pass
-
-        if (menu_option == "0"):
-            sys.exit()
 
 
 main()
