20917bf2e8ccf01869056ce2d50549f148cae841
Edith Vidal <ecsr.vidal@gmail.com>
2025-02-26 20:40:24 -0300

    Deixando de utilizr GraphGenerator no BoxplotMenu

--- src/menu/boxplot_menu.py
+++ src/menu/boxplot_menu.py
@@ -1,6 +1,7 @@
 from graph.graph_generator import GraphGenerator
 from menu.analysis_type_menu import AnalysisTypeMenu
 from const.analysis_type_option import AnalysisTypeOption
+from graph.box_plot import BoxPlot
 
 
 class BoxplotMenu():
@@ -19,22 +20,30 @@ class BoxplotMenu():
             print("Coluna não encontrada")
             return
         if analysis_option == AnalysisTypeOption.UNIVARIATE:
-            self.graph_generator.draw_box_plot(
-                dataframe=self.df,
+            box_plot = BoxPlot(
+                data=self.df,
                 x=None,
                 y=axis_x,
-                src=axis_x
+                title=f'Boxplot (bivariada) {axis_x}',
+                src=axis_x,
+                palette="viridis"
             )
+            box_plot.draw()
+            box_plot.save()
         elif analysis_option == AnalysisTypeOption.BIVARIATE:
             axis_y = input("Informe a coluna secundária: ")
             if axis_y not in self.df.columns:
                 print("Coluna não encontrada")
                 return
-            self.graph_generator.draw_box_plot(
-                dataframe=self.df,
+            box_plot = BoxPlot(
+                data=self.df,
                 x=axis_x,
                 y=axis_y,
-                src=f'{axis_x}_{axis_y}'
+                title=f'Boxplot (univariada) {axis_x}_{axis_y}',
+                src=f'{axis_x}_{axis_y}',
+                palette="viridis"
             )
+            box_plot.draw()
+            box_plot.save()
         else:
             print("Tipo de análise inválido")
